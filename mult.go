0d656317f50d5888816c018623dc983244de1f8f
karlyuk95 <karlyuk95@gmail.com>
2020-04-09 23:53:14 +0300

    Multiples of 3 or 5

---
+++ mult.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	f "fmt"
+)
+
+func Multiple3And5(number int) int {
+
+	//variant #1
+	//arr := make([]int, number)
+	//sum := 0
+
+	// for i := 0; i < len(arr); i++ {
+	// 	arr[i] += i
+	// }
+
+	// for i := 0; i < len(arr); i++ {
+	// 	if arr[i]%3 == 0 || arr[i]%5 == 0 {
+	// 		sum += arr[i]
+	// 		f.Println(sum)
+	// 	}
+
+	// }
+
+	//variant 2
+	sum := 0
+
+	for x := 0; x < number; x++ {
+		if x%3 == 0 || x%5 == 0 {
+			sum += x
+		}
+	}
+
+	return sum
+}
+
+// 	for i, num := range arr {
+// 		f.Println("index:", i, "value:", num)
+// 	}
+
+func main() {
+	result := Multiple3And5(10)
+	f.Println("The result of multiple of 3 and 5 equals ", result)
+}
